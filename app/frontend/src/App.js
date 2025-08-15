33a03be637de35717dc3cd31e44623165909a4df
chekuribharath <chekuri.bharath141@gmail.com>
2025-08-15 18:09:41 +0000

    Initial commit

---
+++ app/frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/message')
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>Full Stack Boilerplate</h1>
+      <p>Backend says: {message}</p>
+    </div>
+  );
+}
+
+export default App;
